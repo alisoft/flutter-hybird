11e078c5e8cbb3324931e48395f62090e7e3a2bf
see <upcwangying@126.com>
2022-04-25 21:27:18 +0800

    fix: android splash

--- module_android/app/src/main/java/dev/alisoft/apps/module_android/MainActivity.java
+++ module_android/app/src/main/java/dev/alisoft/apps/module_android/MainActivity.java
@@ -1,6 +1,11 @@
 package dev.alisoft.apps.module_android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.window.SplashScreenView;
+
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.WindowCompat;
 
 import io.flutter.embedding.android.FlutterActivity;
 
@@ -8,6 +13,18 @@ public class MainActivity extends FlutterActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            getSplashScreen()
+                    .setOnExitAnimationListener(
+                            SplashScreenView::remove);
+        } else {
+            SplashScreen.installSplashScreen(this);
+        }
+
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
     }
